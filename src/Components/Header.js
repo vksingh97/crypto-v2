83e0bc37b64d86dd21c5bf99776c3aa5268eee85
Vivek <viveksingh.singh50@gmail.com>
2022-03-14 23:15:30 +0530

    initilised App and added header component

---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Select,
+} from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography>My BTC</Typography>
+          <Select variant="outlined" style={}>
+            <MenuItem>USD</MenuItem>
+            <MenuItem>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
