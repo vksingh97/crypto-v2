32a305c56c637850ff1889bd8c6f7f0198e9d50b
Vivek <viveksingh.singh50@gmail.com>
2022-03-15 01:09:26 +0530

    added alice-carousel and populated crypto data to carousel

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Homepage } from "../Pages/Homepage";
 export { default as CoinPage } from "../Pages/CoinPage";
-export { default as Banner } from "./Banner";
+export { default as Banner } from "./Banner/Banner";
+export { default as Carousel } from "./Banner/Carousel";
